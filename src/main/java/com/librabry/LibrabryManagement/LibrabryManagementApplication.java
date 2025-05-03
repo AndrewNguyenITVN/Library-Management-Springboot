71739dabcfef34a91294fe09b56d78463799403e
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2025-05-03 19:07:54 +0700

    first commit

---
+++ src/main/java/com/librabry/LibrabryManagement/LibrabryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.librabry.LibrabryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrabryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrabryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librabry/LibrabryManagement/LibrabryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librabry.LibrabryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrabryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
