7bc6f58238efebb2f221348dbb991d4c9ff7d2d0
Nhut Nguyen <nhutb2205896@student.ctu.edu.vn>
2025-05-17 22:23:06 +0700

    borrowing

---
+++ src/main/java/com/library/LibraryManagement/dto/BorrowingDTO.java
@@ -0,0 +1,70 @@
+package com.library.LibraryManagement.dto;
+
+import java.util.Date;
+
+public class BorrowingDTO {
+
+    private int readerId;
+    private int bookId;
+    private String bookTitle;
+    private Date borrowedAt;
+    private Date returnedAt;
+    private Boolean status;
+    private Date dueDate;
+
+    public int getReaderId() {
+        return readerId;
+    }
+
+    public void setReaderId(int readerId) {
+        this.readerId = readerId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public Date getBorrowedAt() {
+        return borrowedAt;
+    }
+
+    public void setBorrowedAt(Date borrowedAt) {
+        this.borrowedAt = borrowedAt;
+    }
+
+    public Date getReturnedAt() {
+        return returnedAt;
+    }
+
+    public void setReturnedAt(Date returnedAt) {
+        this.returnedAt = returnedAt;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/main/java/com/library/LibraryManagement/service/imp/BorrowBookServiceImp.java
@@ -0,0 +1,5 @@
+package com.library.LibraryManagement.service.imp;
+
+public interface BorrowBookServiceImp {
+
+}
